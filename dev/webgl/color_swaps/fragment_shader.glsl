da56f64bac6653b5680cecd9e7f75874462a0708
Evan Callaway <evan@weaveup.com>
2023-10-18 15:42:19 +0000

    Improve color swapping shader

--- dev/webgl/color_swaps/fragment_shader.glsl
+++ dev/webgl/color_swaps/fragment_shader.glsl
@@ -1,31 +1,27 @@
+#define MAX_COLORS 24
+#define COLOR_TOLERANCE 0.000001
+
 precision highp float;
 varying vec2 vTex;
 uniform sampler2D sampler0;
 
-uniform vec4 u_new_colors[24];
-uniform vec4 u_original_colors[24];
+uniform vec4 u_new_colors[MAX_COLORS];
+uniform vec4 u_original_colors[MAX_COLORS];
 uniform int u_colors_count;
 
-float colorTolerance = 0.001;
-
-vec4 colorSwap(vec4 oldColor[24], vec4 newColor[24], vec4 imageColor) {
-  for (int i = 0; i < 24; i++) {
+vec4 colorSwap(vec4 oldColor[MAX_COLORS], vec4 newColor[MAX_COLORS], vec4 imageColor) {
+  for (int i = 0; i < MAX_COLORS; i++) {
     if (i < u_colors_count) {
-      vec4 oldC = oldColor[i];
-      float thisDist = sqrt(
-        pow(oldC.x - imageColor.x, 2.0) +
-        pow(oldC.y - imageColor.y, 2.0) +
-        pow(oldC.z - imageColor.z, 2.0)
-      );
-      if (thisDist < colorTolerance) {
-        return newColor[i];
-      }
+      // Calculate distance^2 to avoid using an expensive square root operation
+      vec4 delta = oldColor[i] - imageColor;
+      float distSquared = dot(delta, delta);
+
+      if (distSquared < COLOR_TOLERANCE) return newColor[i];
     }
   }
-  return imageColor;
+  return imageColor; // No Match, keep the original color
 }
 
 void main(void){
-  vec4 imageColor = texture2D(sampler0, vTex);
-  gl_FragColor = colorSwap(u_original_colors, u_new_colors, imageColor);
+  gl_FragColor = colorSwap(u_original_colors, u_new_colors, texture2D(sampler0, vTex));
 }
