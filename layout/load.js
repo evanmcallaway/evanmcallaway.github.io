c7dbcc7a3701c003b09daa319c076bf69ce63019
Evan Callaway <evan@weaveup.com>
2023-10-18 18:11:40 +0000

    Improve janky website loading behavior

--- layout/load.js
+++ layout/load.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-  fetch(`/layout/header.html`).then(file => {
+  headerPromise = fetch(`/layout/header.html`).then(file => {
     file.text().then(content => {
       document.body.insertAdjacentHTML('afterbegin', content)
     })
   })
 
-  fetch(`/layout/footer.html`).then(file => {
+  footerPromise = fetch(`/layout/footer.html`).then(file => {
     file.text().then(content => {
       document.body.insertAdjacentHTML('beforeend', content)
     })
+  })
+
+  Promise.all([headerPromise, footerPromise]).then(function() {
+    document.body.style.opacity = '1'
   })
 })
